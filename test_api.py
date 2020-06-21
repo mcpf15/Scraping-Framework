2e3f8b975a8dcc8c89a55010786aed7deddfc882
danmelles <daniel.r.melles@gmail.com>
2020-06-21 13:58:04 -0700

    Example of API access

--- test_api.py
+++ test_api.py
@@ -9,9 +9,17 @@ params = {
         "f": "json",
         "where": "1=1"
         }
-resp = requests.get("https://services.arcgis.com/afSMGVsC7QlRK1kZ/arcgis/rest/services/Police_Incidents_2018_PIMS/FeatureServer/0/query", params=params)
+# resp = requests.get("https://services.arcgis.com/afSMGVsC7QlRK1kZ/arcgis/rest/services/Police_Incidents_2018_PIMS/FeatureServer/0/query", params=params)
+payload = {
+    "catalog": {
+        "groupIds" : "any(79606f50581f4a33b14a19e61c4891f7)" 
+    }
+}
+resp = requests.post("https://opendata.arcgis.com/api/v3/search", json=payload)
 jsn = resp.json()
-
-for obj in jsn['features']:
-    print(obj['attributes'])
+for obj in jsn['data']:
+    print('{}:{}'.format(obj['attributes']['name'], obj['attributes']['url']))
+    resp2 = requests.get(obj['attributes']['url'] + '/query', params=params)
+    for record in resp2.json().get('features', []):
+        print(record['attributes'])
 
