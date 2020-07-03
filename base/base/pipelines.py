86f44d8a2d5e9df2db35cfe66232bae22b846680
danmelles <daniel.r.melles@gmail.com>
2020-07-03 11:20:29 -0700

    Added comments and file storage of datasets

--- base/base/pipelines.py
+++ base/base/pipelines.py
@@ -4,8 +4,11 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import json
 
 class BasePipeline:
-    def process_item(self, item, spider):
-        return item
+    def close_spider(self, spider):
+        for name, records in spider.datasets.items():
+            # Store each dataset as its own file
+            with open('{}.json'.format(name), 'w') as f:
+                json.dump(records, f, indent=4)
--- base/base/spiders/example.py
+++ base/base/spiders/example.py
@@ -7,35 +7,57 @@ from scrapy.http.request.json_request import JsonRequest
 class ExampleSpider(scrapy.Spider):
     name = 'example'
     allowed_domains = ['ci.minneapolis.mn.us', 'services.arcgis.com', 'opendata.arcgis.com']
+   
+    def __init__(self, *args, **kwargs):
+        super(ExampleSpider, self).__init__(*args, **kwargs)
+        # Mapping from name of dataset to data entries
+        self.datasets = {}
 
     def parse(self, response):
         jsn = json.loads(response.text)
+        # Params used to get data entries from API
         params = {
-                "outFields": "*",
-                "returnGeometry": "false",
-                "resultOffset": 0,
-                "resultRecordCount": 1,
-                "f": "json",
-                "where": "1=1"
-                }
+            "outFields": "*",
+            "returnGeometry": "false",
+            "resultOffset": 0,
+            "resultRecordCount": 10,
+            "f": "json",
+            "where": "1=1"
+        }
         for obj in jsn['data']:
+            # Attributes contain the name of the dataset and URL, among other things
             attrs = obj['attributes']
-            print('{}:{}'.format(attrs'name'], attrs['url']))
-            yield scrapy.Request(url=attrs['url'] + '/query?' + urllib.parse.urlencode(params), callback=self.parse_records)
+            # Request data entries from API
+            yield scrapy.Request(
+                url=attrs['url'] + '/query?' + urllib.parse.urlencode(params),
+                callback=self.parse_records,
+                meta={'name': attrs['name']}
+            )
 
     def parse_records(self, response):
         jsn = json.loads(response.text)
+        # 'features' contains data entries
         for record in jsn['features']:
-            yield record['attributes']
+            attrs = record['attributes']
+            # Add dataset to mapping if not there 
+            dataset = self.datasets.setdefault(response.meta['name'], [])
+            # Add data entry to dataset
+            dataset.append(attrs)
 
     def start_requests(self):
+        # ID of Minneapolis datasets. Gotten from Chrome Dev Tools
         payload = {
-                "catalog": {
-                    "groupIds": "any(79606f50581f4a33b14a19e61c4891f7)"
-                    }
-                }
-        return [JsonRequest(url='https://opendata.arcgis.com/api/v3/search', method='POST', data=payload)]
-
+            "catalog": {
+                "groupIds": "any(79606f50581f4a33b14a19e61c4891f7)"
+            }
+        }
+        return [
+            JsonRequest(
+                url='https://opendata.arcgis.com/api/v3/search', 
+                method='POST', 
+                data=payload
+            )
+        ]
 
 
 
